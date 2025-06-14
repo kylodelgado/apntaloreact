10dd87bca92dd0b6cb10af5bb33a8e3141ec6f10
kylo.delgado <kylo.delgado@gmail.com>
2025-06-14 01:00:22 -0400

    google biding works

--- ios/apntaloreact/AppDelegate.mm
+++ ios/apntaloreact/AppDelegate.mm
@@ -1,6 +1,8 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+// 1. Import IronSource SDK
+#import <IronSource/IronSource.h>
 
 @implementation AppDelegate
 
@@ -10,6 +12,23 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+#if DEBUG
+  // 2. Enable IronSource Test Suite (set meta data BEFORE init)
+  [IronSource setMetaDataWithKey:@"is_test_suite" value:@"enable"];
+
+  // 3. Initialize IronSource (replace with your actual app key)
+  [IronSource initWithAppKey:@"2260b5485" delegate:nil];
+
+  // 4. Launch the Test Suite after initialization
+  // (No delegate, so we launch immediately for test purposes)
+  dispatch_async(dispatch_get_main_queue(), ^{
+    UIViewController *rootVC = [UIApplication sharedApplication].delegate.window.rootViewController;
+    if (rootVC) {
+      [IronSource launchTestSuite:rootVC];
+    }
+  });
+#endif
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
